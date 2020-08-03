d0998b22cc31d9311336ca524655cf3672be55bc
Debian <bitnami@ip-172-31-62-7.ec2.internal>
2020-08-03 16:23:37 +0000

    new file:   app.js 	new file:   bin/www 	new file:   models/CSSE_COVID_19_daily_reports.js 	new file:   models/CSSE_COVID_19_daily_reports_US.js

---
+++ models/CSSE_COVID_19_daily_reports.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const CSSE_COVID_19_daily_reports_US_Model = new Schema({
+
+	Province_State: {
+		type String,
+	},
+	Country_Region: {
+		type: String
+	},
+	Last_Update: {
+		type: String
+	},
+	Lat: {
+		type: Number 
+	},
+	Long_: {
+		type: Number 
+	},
+	Confirmed: {
+		type: Number 
+	},
+	Deaths: { 
+		type: Number
+	},
+	Recovered: {
+		type: Number 
+	},
+	Active: { 
+		type: Number 
+	},
+	FIPS: { 
+		type: Number 
+	},
+	Incident_Rate: { 
+		type: Number 
+	},
+	People_Tested: { 
+		type: Number 
+	},
+	People_Hospitalized: {
+		type: Number
+	},
+	Mortality_Rate: { 
+		type: Number 
+	},
+	UID: { 
+		type: Number
+	},
+	ISO3: {
+		type: String
+	},
+	Testing_Rate: { 
+		type: Number
+	},
+	Hospitalization_Rate : {
+		type: Number
+	},
+};
+
+module.exports = mongoose.model('CSSE_COVID_19_daily_reports_US', CSSE_COVID_19_daily_reports_US_Model)
---
+++ models/CSSE_COVID_19_daily_reports_US.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const CSSE_COVID_19_daily_reports_US_Model = new Schema({
+
+	FIPS: {
+		type String,
+	},
+	Admin2: {
+		type: String
+	},
+	Province_State: {
+		type: String
+	},
+	Country_Region: {
+		type: Number 
+	},
+	Lat: {
+		type: Number 
+	},
+	Long_: {
+		type: Number 
+	},
+	Confirmed: { 
+		type: Number
+	},
+	Recovered: {
+		type: Number 
+	},
+	Active: { 
+		type: Number 
+	},
+	combined_Key: { 
+		type: String 
+	},
+	Incidence_Rate: { 
+		type: Number 
+	},
+	Case-Fatality_Ratio: { 
+		type: Number 
+	},
+};
+
+module.exports = mongoose.model('CSSE_COVID_19_daily_reports_US', CSSE_COVID_19_daily_reports_US_Model)
