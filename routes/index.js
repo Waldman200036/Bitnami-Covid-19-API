8653dca0f670ff544ee6be9846f68158247283e1
Waldman200036 <Waldman200036@gmail.com>
2020-08-06 20:42:34 -0500

    Text Change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'COVID-19' });
 });
 
 module.exports = router;
